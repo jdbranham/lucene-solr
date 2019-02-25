7945536ba848c4e6a35b1ea4522ed1445368d4f2
Jeremy <jdbranham@hotmail.com>
2019-02-25 07:42:32 -0600

    Revert "Add next minor release version 8.1.0"
    This reverts commit db01124eea168ccea6a0bfc13f592de7b754e4c8.

--- lucene/core/src/java/org/apache/lucene/util/Version.java
+++ lucene/core/src/java/org/apache/lucene/util/Version.java
@@ -40,13 +40,6 @@ public final class Version {
   public static final Version LUCENE_8_0_0 = new Version(8, 0, 0);
 
   /**
-   * Match settings and bugs in Lucene's 8.1.0 release.
-   * @deprecated Use latest
-   */
-  @Deprecated
-  public static final Version LUCENE_8_1_0 = new Version(8, 1, 0);
-
-  /**
    * Match settings and bugs in Lucene's 9.0.0 release.
    * <p>
    * Use this to get the latest &amp; greatest settings, bug
